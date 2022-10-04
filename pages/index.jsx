ea628f34ce2e61ac9de653f84afe5801dc1b6e49
George <gbh218@gmail.com>
2022-10-04 19:52:28 -0400

    Add size to heading

--- pages/index.jsx
+++ pages/index.jsx
@@ -140,7 +140,7 @@ export default function Home() {
           width="100%"
           maxWidth={maxWidth}
         >
-          <Heading mb={2} textAlign='center'>
+          <Heading size="lg" mb={2} textAlign='center'>
             How Smart Invoice works
           </Heading>
           <Text textAlign="center">
